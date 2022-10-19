aa50df073a46aa5d9d87b302a3d9b9d44f6f3013
Henrique Dreyer <henriquedreyer@gmail.com>
2022-10-19 08:14:19 -0300

    First Commit

---
+++ src/main/java/com/dreyer/pethealth/api/PethealthApiApplication.java
@@ -0,0 +1,13 @@
+package com.dreyer.pethealth.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PethealthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PethealthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreyer/pethealth/api/PethealthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreyer.pethealth.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PethealthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
